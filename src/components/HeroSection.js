8006b4ec8239503e34c7ea6b9a218d240b6d42f7
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2022-04-29 01:36:37 +0300

    HeaderSection update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
 import ReactFlagsSelect from "react-flags-select";
+import { FaFacebook } from "react-icons/fa";
 
 export default function HeroSection() {
+  const [selected, setSelected] = useState("TR");
 
-  const[selected,setSelected]=useState('TR');
-
-  const phones={
-    US:'+1',
-    DE:'+50',
-    TR:'+90',
-    IT:'+7',
-    IN:'+15'
-  }
+  const phones = {
+    US: "+1",
+    DE: "+50",
+    TR: "+90",
+    IT: "+7",
+    IN: "+15",
+  };
 
   const settings = {
     dots: false,
@@ -21,6 +21,10 @@ export default function HeroSection() {
     arrows: false,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    speed:500,
+    autoplaySpeed: 3500,
+    cssEase:"linear"
   };
 
   return (
@@ -61,18 +65,33 @@ export default function HeroSection() {
           <h4 className="text-primary-brand-color text-center mb-4 font-semibold">
             Giriş yap veya kayıt ol
           </h4>
-          <div className="flex gap-x-2">
-            <ReactFlagsSelect
-              countries={Object.keys(phones)}
-              customLabels={phones}
-              onSelect={code => setSelected(code)}
-              selected={selected}
-              className="flag-select"
-            />
-            <label className="flex-1 relative block">
-              <input required className="h-14 px-4 border-2 peer text-sm pt-2 border-gray-200 rounded w-full transition-colors hover:border-primary-brand-color focus:border-primary-brand-color outline-none"/>
-              <span className="transition-all peer-focus:h-7 peer-focus:text-xs peer-focus:text-primary-brand-color absolute text-sm peer-valid:h-7 peer-valid:text-xs peer-valid:text-primary-brand-color text-gray-500 top-0 left-0 h-full px-4 flex items-center">Telefon Numarası</span>
-            </label>
+          <div className="grid gap-y-3">
+            <div className="flex gap-x-2">
+              <ReactFlagsSelect
+                countries={Object.keys(phones)}
+                customLabels={phones}
+                onSelect={(code) => setSelected(code)}
+                selected={selected}
+                className="flag-select"
+              />
+              <label className="flex-1 group relative block cursor-pointer">
+                <input
+                  required
+                  className="h-14 px-4 border-2 peer text-sm pt-2 border-gray-200 rounded w-full cursor-pointer transition-colors group-hover:border-primary-brand-color focus:border-primary-brand-color outline-none"
+                />
+                <span className="transition-all peer-focus:h-7 peer-focus:text-xs peer-focus:text-primary-brand-color absolute text-sm peer-valid:h-7 peer-valid:text-xs peer-valid:text-primary-brand-color text-gray-500 top-0 left-0 h-full px-4 flex items-center">
+                  Telefon Numarası
+                </span>
+              </label>
+            </div>
+            <button className="bg-brand-yellow text-primary-brand-color transition-colors hover:bg-primary-brand-color hover:text-brand-yellow h-12 flex items-center justify-center rounded-md w-full text-sm font-semibold ">
+              Telefon numarası ile devam et
+            </button>
+            <hr className="h-[1px] bg-gray-300 my-2" />
+            <button className="bg-blue-700 bg-opacity-10 px-4 text-blue-700 text-opacity-80 transition-colors hover:bg-blue-700 hover:text-white h-12 flex items-center rounded-md w-full text-sm font-semibold ">
+              <FaFacebook size={24} />
+              <span className="mx-auto">Facebook ile devam et</span>
+            </button>
           </div>
         </div>
       </div>
